3887beec6573128b431435235c4a1f7e781acc8b
Reinhard kiprop <chemorein24@gmail.com>
2022-12-03 14:57:38 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,207 @@
-# Flaticus
-Created with CodeSandbox
+////////////////////////////////////////////////////
+// HAVE JSON SERVER RUNNING FIRST!
+////////////////////////////////////////////////////
+// https://codesandbox.io/s/1pxhw?file=/src/api.json
+////////////////////////////////////////////////////
+####################
+
+## Changelog
+
+####################
+
+#### 8/23.1 (current)
+
+1. I added filter by first letter buttons. Their functionality is to float left characters with the first letter of their name matching. Filter buttons were placed at top left for convience because that's where they float to and your pointer will be nearby to choose which you want.
+
+2. Buttons auto populate when you first load the page, and when you add and delete characters. It keeps in sync. Example if there are no more "A" characters, the A button will disappear.
+
+3. I added a scrollToFocus onto the vote, add, edit buttons to pull the view where it needs to be without user needing to scroll. Possibly in future I will add a cycle left and right buttons hovering the character bar, and a jump to top of screen in the right hand corner.
+
+_Misc._ Truthfully what's less resource intensive than repopulating the buttons as they change would be to hard code the letters that can be used and you hide/shrink buttons with nothing to float outside of view. But that would be less open than the method I did it in you can use any character in any language and the top bar should pull it in.
+
+#### 8/22.3
+
+1. I've added the ability to replace the image with a copy/paste URL during edit
+
+2.......!!!BUG: when I EDIT THEN ADD cuties THEN delete my array becomes offset by that amount added. need to make sure edit properly adjust the array. most likely need to do a return Add-delete-edit-save no change-edit-change picture-save-add-BUG
+-FIXed
+
+3.....!!!BUG: check toggle save after edit, does not always change back to edit icon, happens only when you toggle to change pictures and then press save -FIXED
+
+#### 8/22.2
+
+1. BIG UPDATE - added toggle buttons for displaying the vote and add form, moved teh delete key function to a toggle button, and allow users through an edit button to manually edit the amoutn of votes and name.
+
+2. all editted data is displayed realtime to the client without doing unneeded GET request from the dataabase, also teh database is updated to match.
+
+3. I practiced declaring new variables inside an array. so those variables that eslint thinks doesnt exist, do exist.
+
+#### 8/21
+
+1. You can now scroll the top bar by click, hold, and dragging in the blue section below the cuties
+
+2. Removed unnecessary properties and keys from being pushed to JSON when using updateData when changing votes
+
+3. Adjust some margins, padding, and heights in CSS
+
+4. Rearranged some divs in HTML
+
+5. Added a feature to hide addForm on initial load, viewable by pressing +, to keep viewers focused on featuredCharacter on first sight rather than what the text boxes are out of sight. Left it so that it does not hide the addForm after used. Incase user wants to add multiple. Ideally the form would be a modal
+
+6. Added a couple more placeholder pictures and a randomizer to select one of them in place of user providing no image
+
+# Flatacuties
+
+Today you'll be building an app for voting for the cutest animal! You will be
+using a local API and building out the frontend for our app, Flatacuties.
+
+## Setup
+
+Run this command to get the backend started:
+
+```sh
+json-server --watch db.json
+```
+
+Test your server by visiting this route in the browser:
+
+[http://localhost:3000/characters](http://localhost:3000/characters)
+
+Then, open the `index.html` file on your browser to run the application.
+
+Write your code in the `src/index.js` file. The base URL for your API will be
+[http://localhost:3000](http://localhost:3000).
+
+## Deliverables
+
+As a user, I can:
+
+1. See all characters names in a `div` with the id of `"character-bar"`. Create
+   a `span` tag with the character's name and add it the `div#character-bar`
+   once you have retrieved the character data from the server. You will need to
+   make a GET request to the following endpoint to retrieve the character data:
+
+   ```txt
+   GET /characters
+
+   Example Response:
+   [
+    {
+      "id": 1,
+      "name": "Mr. Cute",
+      "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
+      "votes": 0
+    },
+    {
+      "id": 2,
+      "name": "Mr. Monkey",
+      "image": "https://thumbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
+      "votes": 0
+    },
+    ...
+   ]
+   ```
+
+2. When the character in the `div#character-bar` is clicked, display the
+   character's details in the `div#detailed-info`. You can either use the
+   character information from your first request, or make a new request to this
+   endpoint to get the character's details:
+
+   ```txt
+   GET /characters/:id
+
+   Example Response:
+   {
+    "id": 1,
+    "name": "Mr. Cute",
+    "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
+    "votes": 0
+   }
+   ```
+
+3. When the `form#votes-form` is submitted, add the number of votes from
+   the input field to the character displayed in the `div#detailed-info`. **No
+   persistence is needed**.
+
+### Bonus Deliverables
+
+These bonus deliverables are here if you want an extra challenge and won't
+affect your score. **Make sure to commit your work to save your progress before
+attempting the bonus deliverables!**
+
+In the `index.html` file, there is some additional HTML that is currently
+commented out below the Reset Votes button. Remove the comments (delete the
+`<!--` and `-->` code around the elements) so you can complete the bonus
+deliverables.
+
+1. When the Reset Votes button is clicked, reset the votes back to 0.
+
+2. When the `form#character-form` is submitted, add a new character to the
+   `div#character-bar`. The new character in the character bar should behave the
+   same as the other characters when clicked (its details should be displayed
+   below, and it should have functionality to add votes).
+
+3. In addition to adding the character to the `div#character-bar` upon
+   submitting the form, the character's details should show up immediately in
+   the `div#detailed-info`.
+
+### Extra Bonus
+
+These extra bonus deliverables involve using `fetch` to update data on the
+`json-server` backend by using `POST`, `PATCH`, and `DELETE` requests. These are
+meant for an extra, extra challenge and won't affect your grade. **Make sure to
+commit your work to save your progress before attempting the extra bonus
+deliverables!**
+
+1. When a user adds or resets the votes for a character, in addition to
+   displaying the updated votes on the page, the votes should **also** be
+   updated on the server. You will need to make a request that follows this
+   structure:
+
+   ```txt
+   PATCH /characters/:id
+
+   Request Headers: {
+     Content-Type: application/json
+   }
+
+   Request Body: {
+     "votes": 100
+   }
+   ----
+
+   Example Response: {
+     "id": 1,
+     "name": "Mr. Cute",
+     "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
+     "votes": 100
+   }
+   ```
+
+2. When a user adds a new character to the page using the character form, in
+   addition to having the character show up on the page, it should **also** be
+   saved to the server. You will need to make a request that follows this
+   structure:
+
+   ```txt
+   POST /characters
+
+   Request Headers: {
+     Content-Type: application/json
+   }
+
+   Request Body: {
+     "name": "Character Name",
+     "image": "https://example.com/my-image.gif",
+     "votes": 0
+   }
+
+   ----
+
+   Example Response: {
+     "id": 6,
+     "name": "Character Name",
+     "image": "https://example.com/my-image.gif",
+     "votes": 0
+   }
+   ```
---
+++ oldVersions/index_old_8-22.3.js
@@ -0,0 +1,475 @@
+// // 1. See all characters names in a `div` with the id of `"character-bar"`. Create
+// // a `span` tag with the character's name and add it the `div#character-bar`
+// // once you have retrieved the character data from the server. You will need to
+// // make a GET request to the following endpoint to retrieve the character data:
+// //const BASE_URL = "http://localhost:3000/characters"
+
+//////////////////////////////////////////////////////////////////////
+//                  HAVE JSON SERVER RUNNING FIRST!                 //
+/////////////////                                    /////////////////
+//       https://codesandbox.io/s/zpdu6?file=/src/api.json          //
+//////////////////////////////////////////////////////////////////////
+
+//LAST UPDATE 8/22/2021 in README
+const BASE_URL = "https://zpdu6.sse.codesandbox.io/characters";
+
+const characterBar = document.getElementById("character-bar");
+const voteForm = document.getElementById("votes-form");
+const addCharacterForm = document.getElementById("character-form");
+const addCharacterFormH4 = document.querySelector("#character-form h4");
+const detailInfoName = document.querySelector("#detailed-info #name");
+const detailBoxName = document.getElementById("name");
+const detailBoxImage = document.getElementById("image");
+const detailBoxVotes = document.getElementById("vote-count");
+const detailedInfoDiv = document.getElementById("image-container");
+let featuredCharacter;
+let clientSideRealtimeData;
+//I try using a foreach loop to create listBtns and make them variables pushed into toggleBtnsArray. Which are accessible by the global scope
+const toggleBtnsToCreate = [
+  "votesFeaturedToggleBtn",
+  "addFeaturedToggleBtn",
+  "releaseFeaturedToggleBtn",
+  "editFeaturedToggleBtn"
+];
+let toggleBtnsArray = [];
+const toggleBtnContainer = document.createElement("DIV");
+toggleBtnContainer.id = "toggleBtnContainer";
+toggleBtnContainer.style = "position: fixed; top: 30%; left: 75vw; width: 0vw;";
+document.body.append(toggleBtnContainer);
+const oLToggleBtns = document.createElement("OL");
+toggleBtnContainer.append(oLToggleBtns);
+addCharacterForm.style = "margin-top: 80px;";
+addCharacterFormH4.style = "margin-bottom: 20px;";
+init();
+
+function init() {
+  getData(BASE_URL);
+  voteForm.addEventListener("submit", addVotes);
+  toggleBtnsToCreate.forEach(createToggleBtns);
+  addCharacterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    refreshDataBeforeEdit(BASE_URL);
+  });
+}
+
+function getData(BASE_URL) {
+  fetch(BASE_URL)
+    .then((resp) => resp.json())
+    .then((data) => {
+      loadFirstChild(data);
+      data.forEach(appendDisplayNames);
+      clientSideRealtimeData = data;
+      return clientSideRealtimeData;
+    });
+}
+
+function refreshDataBeforeEdit(BASE_URL) {
+  fetch(BASE_URL)
+    .then((resp) => resp.json())
+    .then((data) => {
+      addNewCharacter(data);
+    });
+}
+
+function appendDisplayNames(data) {
+  characterBar.className = "scrolling-wrapper";
+  const cBDiv = document.createElement("DIV");
+  cBDiv.className = "card";
+  cBDiv.id = `cBD${data.id}`;
+  const cBBreak = document.createElement("BR");
+  const cBImage = document.createElement("IMG");
+  cBImage.src = data.image;
+  cBImage.style = "height: 100px; width: 100px;";
+  const cBAnchor = document.createElement("A");
+  cBAnchor.href = "#";
+  cBAnchor.id = `cBA${data.id}`;
+  cBAnchor.style = `text-decoration:none; text-align: center;`;
+  const cBSpan = document.createElement("SPAN");
+  cBSpan.style = "color: white;";
+  cBSpan.textContent = data.name;
+  characterBar.append(cBDiv);
+  cBDiv.append(cBAnchor);
+  cBAnchor.append(cBImage);
+  cBAnchor.append(cBBreak);
+  cBAnchor.append(cBSpan);
+  cBAnchor.addEventListener("click", () => displayClickedChar(data));
+}
+
+// 2. When the character in the `div#character-bar` is clicked, display the
+//    character's details in the `div#detailed-info`.
+
+function loadFirstChild(data) {
+  detailBoxName.textContent = data[0].name;
+  detailBoxImage.src = data[0].image;
+  detailBoxVotes.textContent = data[0].votes;
+  featuredCharacter = data[0];
+  return featuredCharacter;
+}
+
+function displayClickedChar(data) {
+  detailBoxName.textContent = data.name;
+  detailBoxImage.src = data.image;
+  detailBoxVotes.textContent = data.votes;
+  featuredCharacter = data;
+  return featuredCharacter;
+}
+
+// 3. When the `form#votes-form` is submitted, add the number of votes from
+//    the input field to the character displayed in the `div#detailed-info`. **No
+//    persistence is needed**.
+
+function addVotes(event) {
+  event.preventDefault();
+  let voteInput = document.getElementById("votes").value;
+  voteInput === ""
+    ? (voteInput = 1)
+    : (voteInput = document.getElementById("votes").value);
+  let initialVotesCount = parseInt(detailBoxVotes.textContent, 0);
+  let newVoteCount = initialVotesCount + parseInt(voteInput, 0);
+  detailBoxVotes.textContent = newVoteCount;
+  let updateData = {
+    id: `${featuredCharacter.id}`,
+    votes: `${newVoteCount}`
+  };
+  //update clicked votes without refresh because it's using old data
+  updateVotesToDB(updateData);
+  updateHeaderDataAfterVotesChange(newVoteCount);
+  event.target.reset();
+}
+
+function updateHeaderDataAfterVotesChange(newVoteCount) {
+  return (featuredCharacter.votes = newVoteCount);
+}
+
+// 1. When the Reset Votes button is clicked, reset the votes back to 0.
+
+function resetVotes() {
+  let updateData = {
+    id: `${featuredCharacter.id}`,
+    votes: 0
+  };
+  updateVotesToDB(updateData);
+  updateHeaderDataAfterVotesChange(0);
+  detailBoxVotes.textContent = 0;
+}
+
+// 2. When the `form#character-form` is submitted, add a new character to the
+//    `div#character-bar`. The new character in the character bar should behave the
+//    same as the other characters when clicked (its details should be displayed
+//    below, and it should have functionality to add votes).
+// 3. In addition to adding the character to the `div#character-bar` upon
+//    submitting the form, the character's details should show up immediately in
+//    the `div#detailed-info`.
+
+function addNewCharacter(data) {
+  let addCharacterNameInput = document.querySelector("#character-form #name")
+    .value;
+  addCharacterNameInput === ""
+    ? (addCharacterNameInput = Math.floor(Math.random() * 100))
+    : (addCharacterNameInput = document.querySelector("#character-form #name")
+        .value);
+  let addCharacterImageInput = document.getElementById("image-url").value;
+  addCharacterImageInput === ""
+    ? (addCharacterImageInput = `assets/PC-${Math.floor(
+        Math.random() * 9
+      )}.png`)
+    : (addCharacterImageInput = document.getElementById("image-url").value);
+  let newDataAddToDB = {
+    name: `${addCharacterNameInput}`,
+    image: `${addCharacterImageInput}`,
+    votes: "0"
+  };
+  addToDB(newDataAddToDB);
+  let newDataWithID = {
+    id: ++clientSideRealtimeData[clientSideRealtimeData.length - 1].id,
+    name: `${addCharacterNameInput}`,
+    image: `${addCharacterImageInput}`,
+    votes: "0"
+  };
+  clientSideRealtimeData = [...data, { ...newDataWithID }];
+
+  appendDisplayNames(newDataWithID);
+  featuredNewCharacterAfterAdd(newDataWithID);
+  document.querySelector("#character-form #name").value = "";
+  return clientSideRealtimeData;
+}
+
+function featuredNewCharacterAfterAdd(newDataWithID) {
+  detailBoxName.textContent = newDataWithID.name;
+  detailBoxImage.src = newDataWithID.image;
+  detailBoxVotes.textContent = newDataWithID.votes;
+  featuredCharacter = newDataWithID;
+  return featuredCharacter;
+}
+
+function deleteFeaturedCharacter() {
+  document.getElementById(`cBD${featuredCharacter.id}`).remove();
+  let indexFoundAt = findIndexOfFeaturedCharacterInArray(featuredCharacter);
+  clientSideRealtimeData.splice(indexFoundAt, 1);
+  // ^^ABOVE^^ remove the element from clientside, indexed position to displayNextFeaturedCharacter
+  deleteFromDB(featuredCharacter);
+  displayNextFeaturedCharacter(indexFoundAt);
+  return clientSideRealtimeData;
+}
+
+function findIndexOfFeaturedCharacterInArray(featuredCharacter) {
+  //pushing ids to an index so i can index which object gets deleted when client removes character
+  let indexArrayClientCharacters = [];
+  clientSideRealtimeData.forEach((element) =>
+    indexArrayClientCharacters.push(element.id)
+  );
+  const indexFoundAt = indexArrayClientCharacters.findIndex(
+    (element) => element === featuredCharacter.id
+  );
+  return indexFoundAt;
+}
+
+function displayNextFeaturedCharacter(indexBeforeDeletion) {
+  if (indexBeforeDeletion >= 0) {
+    if (clientSideRealtimeData[indexBeforeDeletion] !== undefined) {
+      featuredCharacter = clientSideRealtimeData[indexBeforeDeletion];
+      detailBoxName.textContent =
+        clientSideRealtimeData[indexBeforeDeletion].name;
+      detailBoxImage.src = clientSideRealtimeData[indexBeforeDeletion].image;
+      detailBoxVotes.textContent =
+        clientSideRealtimeData[indexBeforeDeletion].votes;
+      return featuredCharacter;
+    } else {
+      featuredCharacter =
+        clientSideRealtimeData[clientSideRealtimeData.length - 1];
+      detailBoxName.textContent =
+        clientSideRealtimeData[clientSideRealtimeData.length - 1].name;
+      detailBoxImage.src =
+        clientSideRealtimeData[clientSideRealtimeData.length - 1].image;
+      detailBoxVotes.textContent =
+        clientSideRealtimeData[clientSideRealtimeData.length - 1].votes;
+      return featuredCharacter;
+    }
+  } else {
+    featuredCharacter = clientSideRealtimeData[0];
+    detailBoxName.textContent = clientSideRealtimeData[0].name;
+    detailBoxImage.src = clientSideRealtimeData[0].image;
+    detailBoxVotes.textContent = clientSideRealtimeData[0].votes;
+    return featuredCharacter;
+  }
+}
+
+// 1. When a user adds or resets the votes for a character, in addition to
+//    displaying the updated votes on the page, the votes should **also** be
+//    updated on the server. You will need to make a request that follows this
+//    structure:
+
+function addToDB(addData) {
+  fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(addData)
+  })
+    .then((resp) => resp.json())
+    .then((data) => data);
+}
+
+function updateVotesToDB(updateData) {
+  fetch(`${BASE_URL}/${updateData.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(updateData)
+  })
+    .then((resp) => resp.json())
+    .then((data) => data.votes);
+}
+
+function updateEditedFeaturedCharacterToDB(updateData) {
+  fetch(`${BASE_URL}/${updateData.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(updateData)
+  })
+    .then((resp) => resp.json())
+    .then((data) => data);
+}
+
+function deleteFromDB(featuredCharacter) {
+  fetch(`${BASE_URL}/${featuredCharacter.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+function preventDBClearOutCheck() {
+  if (clientSideRealtimeData.length > 1) {
+    deleteFeaturedCharacter();
+  } else {
+    console.log("There always must be ONE!");
+    return clientSideRealtimeData;
+  }
+}
+//Testing out destructure with forEach loop
+function createToggleBtns(element) {
+  const elementLI = document.createElement("LI");
+  elementLI.style = "list-style-type: none;";
+  const elementBtn = document.createElement("BUTTON");
+  elementBtn.id = `${element}`;
+  elementBtn.style = `border-radius: 50px; height:50px; width:50px; background-image: url('assets/${element}.png'); background-repeat: no-repeat; background-position: center;`;
+  oLToggleBtns.append(elementLI);
+  elementLI.append(elementBtn);
+  toggleBtnsArray.push(elementBtn);
+  elementBtn.id === "releaseFeaturedToggleBtn"
+    ? elementBtn.addEventListener("click", preventDBClearOutCheck)
+    : elementBtn.addEventListener("click", () => toggleVisual(elementBtn.id));
+  elementBtn.onmouseenter = function () {
+    elementBtn.style.backgroundColor = "rgb(173, 0, 0)";
+  };
+  elementBtn.onmouseleave = function () {
+    elementBtn.style.backgroundColor = "rgb(49, 109, 179)";
+  };
+  elementBtn.onmousedown = function () {
+    elementBtn.style.backgroundColor = "rgb(126, 0, 0)";
+  };
+}
+
+function toggleVisual(buttonClicked) {
+  buttonClicked === "votesFeaturedToggleBtn" &&
+  voteForm.style.visibility === "hidden"
+    ? (voteForm.style.visibility = "visible")
+    : (voteForm.style.visibility = "hidden");
+  buttonClicked === "addFeaturedToggleBtn" &&
+  addCharacterForm.style.visibility === "hidden"
+    ? (addCharacterForm.style.visibility = "visible")
+    : (addCharacterForm.style.visibility = "hidden");
+  if (buttonClicked === "editFeaturedToggleBtn") editFeaturedCharacter();
+}
+
+function editFeaturedCharacter() {
+  if (document.getElementById("resetBtn") === null) {
+    createEditVoteCountResetBtn();
+    createEditImageBtn();
+    detailInfoName.contentEditable = "true";
+    detailInfoName.style.color = "lightgrey";
+    editFeaturedToggleBtn.style.backgroundImage =
+      "url('assets/saveToggleBtn.png')";
+    editFeaturedToggleBtn.style.backgroundColor = "rgb(173, 0, 0)";
+    votesFeaturedToggleBtn.disabled = true;
+    votesFeaturedToggleBtn.style.backgroundColor = "black";
+    addFeaturedToggleBtn.disabled = true;
+    addFeaturedToggleBtn.style.backgroundColor = "black";
+    releaseFeaturedToggleBtn.disabled = true;
+    releaseFeaturedToggleBtn.style.backgroundColor = "black";
+  } else {
+    clickSaveEdits();
+    reEnableBtns();
+  }
+}
+
+function clickSaveEdits() {
+  const imageContainerP = document.querySelector("#image-container p");
+  if (imageContainerP !== null) {
+    const testURL = imageContainerP.textContent;
+    if (testURL.length > 14) {
+      featuredCharacter.image = imageContainerP.textContent;
+      imageContainerP.remove();
+      detailBoxImage.style = "height: 300px; width:300px;";
+      detailBoxVotes.style.visibility = "visible";
+    } else {
+      detailBoxImage.style = "height: 300px; width:300px;";
+      detailBoxVotes.style.visibility = "visible";
+      imageContainerP.remove();
+    }
+  }
+  toggleOffEdit();
+  let edittedFeatureCharacterData = {
+    id: featuredCharacter.id,
+    name: detailInfoName.textContent,
+    image: featuredCharacter.image,
+    votes: document.getElementById("vote-count").textContent
+  };
+  let indexFoundAt = findIndexOfFeaturedCharacterInArray(
+    edittedFeatureCharacterData
+  );
+  //CHANGE IMAGE AND NAME IN HEADER
+  const featuredCharacterInHeaderImage = document.querySelector(
+    `#cBD${featuredCharacter.id} img`
+  );
+  const featuredCharacterInHeaderName = document.querySelector(
+    `#cBD${featuredCharacter.id} span`
+  );
+  featuredCharacterInHeaderImage.src = edittedFeatureCharacterData.image;
+  featuredCharacterInHeaderName.textContent = edittedFeatureCharacterData.name;
+  //CHANGE display = edittedfeatureCharacterData
+  detailBoxName.textContent = edittedFeatureCharacterData.name;
+  detailBoxImage.src = edittedFeatureCharacterData.image;
+  detailBoxVotes.textContent = edittedFeatureCharacterData.votes;
+  //change full data in clientside array [problem the data the clicker reads from is the initial pull]
+  clientSideRealtimeData.splice(indexFoundAt, 1, edittedFeatureCharacterData);
+  featuredCharacter.name = edittedFeatureCharacterData.name;
+  featuredCharacter.image = edittedFeatureCharacterData.image;
+  featuredCharacter.votes = edittedFeatureCharacterData.votes;
+  //push change to DB
+  updateEditedFeaturedCharacterToDB(edittedFeatureCharacterData);
+  //push updateclientSideRealtimeData
+  return clientSideRealtimeData;
+}
+
+function createEditVoteCountResetBtn() {
+  const resetBtn = document.createElement("BUTTON");
+  resetBtn.id = "resetBtn";
+  resetBtn.textContent = "x";
+  resetBtn.style =
+    "font-size: 12px; border-radius: 10px; border: 1px; margin-top: 0px; height: 20px; width: 20px; flex-direction: column; align-items: center; position: relative; margin-left: 5px; bottom:10px;";
+  resetBtn.addEventListener("click", resetVotes);
+  const h4DetailInfoBox = document.getElementById("vote-countH4");
+  h4DetailInfoBox.append(resetBtn);
+  detailBoxVotes.style.color = "lightgrey";
+  detailBoxVotes.contentEditable = "true";
+}
+
+function createEditImageBtn() {
+  const imageBtn = document.createElement("BUTTON");
+  imageBtn.id = "imageBtn";
+  imageBtn.textContent = "x";
+  imageBtn.style =
+    "font-size: 18px; border-radius: 25px; border: 1px; height: 30px; width: 30px; flex-direction: column; align-items: center; position: absolute;";
+  detailedInfoDiv.insertBefore(imageBtn, detailedInfoDiv.children[1]);
+  imageBtn.addEventListener("click", changeFeaturedImage);
+}
+
+function changeFeaturedImage() {
+  const imageTextBox = document.createElement("P");
+  imageTextBox.style =
+    "overflow: hidden; font-size: 12px; height: 300px; width: 20vw; text-align:center; position: relative; left:41%; text-align:center; display:flex; align-items: center; justify-content:center; background-color: rgb(241, 241, 241);";
+  imageTextBox.contentEditable = "true";
+  detailedInfoDiv.append(imageTextBox);
+  detailBoxImage.style = "max-height: 0px; max-width: 0px;";
+  detailBoxImage.style.visibility = "hidden";
+  document.querySelector("#imageBtn").remove();
+}
+
+function toggleOffEdit() {
+  document.getElementById("resetBtn").remove();
+  detailBoxVotes.style.color = "black";
+  detailInfoName.style.color = "black";
+  detailBoxVotes.contentEditable = "false";
+  detailInfoName.contentEditable = "false";
+  if (document.querySelector("#imageBtn") !== null) {
+    document.querySelector("#imageBtn").remove();
+  }
+}
+
+function reEnableBtns() {
+  editFeaturedToggleBtn.style.backgroundImage =
+    "url('assets/editFeaturedToggleBtn.png')";
+  votesFeaturedToggleBtn.disabled = false;
+  votesFeaturedToggleBtn.style.backgroundColor = "rgb(49, 109, 179)";
+  addFeaturedToggleBtn.disabled = false;
+  addFeaturedToggleBtn.style.backgroundColor = "rgb(49, 109, 179)";
+  releaseFeaturedToggleBtn.disabled = false;
+  releaseFeaturedToggleBtn.style.backgroundColor = "rgb(49, 109, 179)";
+}
